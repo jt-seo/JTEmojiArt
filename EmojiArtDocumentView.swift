cad5a6729f4750d12760e6da838e9d21b9543e50
jt.seo <jt.seo@nexplayer.com>
2020-08-25 21:37:24 +0900

    Implemented zoomToFit feature. Updated to clean code using the OptionalImage.

--- EmojiArtDocumentView.swift
+++ EmojiArtDocumentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 
 struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
+    
+    @State var zoomScale: CGFloat = 1.0
+
     var body: some View {
         VStack {
             HStack {
@@ -23,22 +26,17 @@ struct EmojiArtDocumentView: View {
             }
             GeometryReader { geometry in
                 ZStack {
-                    Color.green
-                    .overlay(
-                        Group {
-                            if self.document.backgroundImage != nil {
-                                Image(uiImage: self.document.backgroundImage!)
-                                    .resizable()
-                            }
+                    Color.white
+                        .overlay(OptionalImage(image: self.document.backgroundImage)
+                            .scaleEffect(self.zoomScale)
+                        )
+                        .edgesIgnoringSafeArea([.bottom, .horizontal])
+                        .onDrop(of: ["public.image", "public.plain-text"], isTargeted: nil) { providers, location in
+                            // convert from the global coordinate to view coordinate.
+                            let origin = geometry.frame(in: .global).origin // the origin of this geometry in the global coordinate system.
+                            let location = location - origin
+                            return self.drop(providers: providers, location: location)
                         }
-                    )
-                    .edgesIgnoringSafeArea([.bottom, .horizontal])
-                    .onDrop(of: ["public.image", "public.plain-text"], isTargeted: nil) { providers, location in
-                        // convert from the global coordinate to view coordinate.
-                        let origin = geometry.frame(in: .global).origin // the origin of this geometry in the global coordinate system.
-                        let location = location - origin
-                        return self.drop(providers: providers, location: location)
-                    }
                     
                     ForEach(self.document.emojis) {emoji in
                         Text(emoji.text)
@@ -46,6 +44,8 @@ struct EmojiArtDocumentView: View {
                             .font(self.document.fontSize(for: emoji))
                     }
                 }
+                .clipped()
+                .gesture(self.fitToZoom(size: geometry.size))
             }
         }
     }
@@ -63,6 +63,30 @@ struct EmojiArtDocumentView: View {
             }
         }
         return found
+    }
+    
+    func fitToZoom(size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                print("fitToZoom")
+                if let image = self.document.backgroundImage, image.size.width > 0 && image.size.height > 0 {
+                    let hZoom = size.width / image.size.width
+                    let vZoom = size.height / image.size.height
+                    self.zoomScale = min(hZoom, vZoom)
+                }
+        }
+    }
+    
+    struct OptionalImage: View {
+        let image: UIImage?
+        
+        var body: some View {
+            Group {
+                if image != nil {
+                    Image(uiImage: image!)
+                }
+            }
+        }
     }
 }
 
